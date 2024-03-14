8cdce2723727ff71e48c4d07cb5f0d6fa6de0875
khunsai <foo@example.com>
2024-03-15 01:37:56 +0630

    Fix: admin login form width issue

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -67,60 +67,65 @@ const Page: FC<any> = () => {
   };
 
   return (
-    <div className="hero is-fullheight is-flex-direction-column is-justify-content-center is-align-items-center">
-      <div className="box" style={{ minWidth: "360px", maxWidth: "480px" }}>
-        <h4 className="title is-5">Login as Administrator</h4>
-        <hr />
-        <form method="post" onSubmit={login}>
-          {alert && (
-            <div className="notification is-danger is-light">
-              <button className="delete" onClick={hideAlert} />
-              {alert}
-            </div>
-          )}
-
-          <TextField
-            type="text"
-            label="Username"
-            name={"uname"}
-            value={unameFieldValue}
-            status={unameFieldAttr.status}
-            help={unameFieldAttr.help}
-            leadingIcon={<Icon path={mdiAccountOutline} size={iconSize} />}
-            onChange={({ currentTarget }) => {
-              mutateUname(currentTarget.value.trim());
-            }}
-          />
-
-          <TextField
-            type="text"
-            label="Password"
-            name={"passwd"}
-            value={passwdFieldValue}
-            status={passwdFieldAttr.status}
-            help={passwdFieldAttr.help}
-            leadingIcon={<Icon path={mdiKeyOutline} size={iconSize} />}
-            onChange={({ currentTarget }) => {
-              mutatePasswd(currentTarget.value.trim());
-            }}
-          />
-
+    <div
+      style={{ height: "100vh" }}
+      className="columns is-mobile is-justify-content-center is-align-items-center"
+    >
+      <div className="column p-0 is-4-desktop is-5-tablet is-10-mobile">
+        <div className="box">
+          <h4 className="title is-5">Login as Administrator</h4>
           <hr />
-          <button
-            className={concatenateStrings([
-              "button is-primary",
-              isPending ? "is-loading" : "",
-            ])}
-            disabled={!(unameFieldAttr.validity && passwdFieldAttr.validity)}
-            style={{ fontFamily: "inherit", width: "100%" }}
-            type="submit"
-          >
-            <span className="icon">
-              <Icon path={mdiLoginVariant} size={iconSize} />
-            </span>
-            <span>Login</span>
-          </button>
-        </form>
+          <form method="post" onSubmit={login}>
+            {alert && (
+              <div className="notification is-danger is-light">
+                <button className="delete" onClick={hideAlert} />
+                {alert}
+              </div>
+            )}
+
+            <TextField
+              type="text"
+              label="Username"
+              name={"uname"}
+              value={unameFieldValue}
+              status={unameFieldAttr.status}
+              help={unameFieldAttr.help}
+              leadingIcon={<Icon path={mdiAccountOutline} size={iconSize} />}
+              onChange={({ currentTarget }) => {
+                mutateUname(currentTarget.value.trim());
+              }}
+            />
+
+            <TextField
+              type="text"
+              label="Password"
+              name={"passwd"}
+              value={passwdFieldValue}
+              status={passwdFieldAttr.status}
+              help={passwdFieldAttr.help}
+              leadingIcon={<Icon path={mdiKeyOutline} size={iconSize} />}
+              onChange={({ currentTarget }) => {
+                mutatePasswd(currentTarget.value.trim());
+              }}
+            />
+
+            <hr />
+            <button
+              className={concatenateStrings([
+                "button is-primary",
+                isPending ? "is-loading" : "",
+              ])}
+              disabled={!(unameFieldAttr.validity && passwdFieldAttr.validity)}
+              style={{ fontFamily: "inherit", width: "100%" }}
+              type="submit"
+            >
+              <span className="icon">
+                <Icon path={mdiLoginVariant} size={iconSize} />
+              </span>
+              <span>Login</span>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
